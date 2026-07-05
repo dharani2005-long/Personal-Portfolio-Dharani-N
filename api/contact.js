efb937e0e6556fd020a9f3784bd215967560e381
Dharani N <136194676+dharani2005-long@users.noreply.github.com>
2026-07-05 19:27:18 +0530

    Update API endpoint URL for form submission

--- api/contact.js
+++ api/contact.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-        "https://api.web3forms.com/submit",
+        "https://dharaninatarajan.vercel.app/submit",
         {
             method: "POST",
             headers: {
