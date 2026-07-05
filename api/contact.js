ee6221bde7f10b554d88387cf19d56c7d019c983
Dharani N <136194676+dharani2005-long@users.noreply.github.com>
2026-07-05 20:00:03 +0530

    temp commit

--- api/contact.js
+++ api/contact.js
@@ -1,40 +1,61 @@
+// export default async function handler(req, res) {
+
+//     if (req.method !== "POST") {
+//         return res.status(405).json({
+//             success: false,
+//             message: "Method Not Allowed"
+//         });
+//     }
+
+//     try {
+
+//         const response = await fetch(
+//             "https://api.web3forms.com/submit",
+//             {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify({
+//                     access_key: process.env.MY_ACCESS_KEY,
+//                     subject: "New message from your portfolio",
+//                     from_name: "Dharani N Portfolio",
+//                     ...req.body
+//                 })
+//             }
+//         );
+
+//         const data = await response.json();
+
+//         return res.status(200).json(data);
+
+//     } catch (error) {
+
+//         return res.status(500).json({
+//             success: false,
+//             message: "Internal Server Error"
+//         });
+
+//     }
+// }
+//-----------------------------
+
+
+
 export default async function handler(req, res) {
-
-    if (req.method !== "POST") {
-        return res.status(405).json({
-            success: false,
-            message: "Method Not Allowed"
-        });
-    }
-
     try {
+        console.log("Access Key:", process.env.MY_ACCESS_KEY);
 
-        const response = await fetch(
-            "https://api.web3forms.com/submit",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    access_key: process.env.MY_ACCESS_KEY,
-                    subject: "New message from your portfolio",
-                    from_name: "Dharani N Portfolio",
-                    ...req.body
-                })
-            }
-        );
+        return res.status(200).json({
+            success: true,
+            keyExists: !!process.env.MY_ACCESS_KEY
+        });
 
-        const data = await response.json();
-
-        return res.status(200).json(data);
-
-    } catch (error) {
-
+    } catch (err) {
+        console.error(err);
         return res.status(500).json({
             success: false,
-            message: "Internal Server Error"
+            message: err.message
         });
-
     }
 }
